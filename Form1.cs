8be064fd6241183e3f8a4bb50f9caf5a157a32e7
e2203080 <e2203080@edu.vamk.fi>
2024-11-04 11:03:44 +0200

    update code

--- Form1.cs
+++ Form1.cs
@@ -14,21 +14,29 @@ namespace checkers
     public partial class Form1 : Form
     {
         private CheckersGame game;
-        private Button[,] boardButtons = new Button[8, 8];  
-        private (int row, int col)? selectedPiece;  
+        private Button[,] boardButtons = new Button[8, 8];
+        private (int row, int col)? selectedPiece;
+
+        
+        private Label currentPlayerLabel = new Label();
 
         public Form1()
         {
             InitializeComponent();
-            game = new CheckersGame();  
-            CreateBoardUI();            
-            RenderBoard();              
+            game = new CheckersGame();
+            CreateBoardUI();
+            RenderBoard();
+
+            
+            currentPlayerLabel.Text = "Current Player: Player " + game.CurrentPlayer;
+            currentPlayerLabel.Location = new System.Drawing.Point(10, 420);  
+            currentPlayerLabel.AutoSize = true;
+            this.Controls.Add(currentPlayerLabel);
         }
 
-        
         private void CreateBoardUI()
         {
-            int tileSize = 50;  
+            int tileSize = 50;
             for (int row = 0; row < 8; row++)
             {
                 for (int col = 0; col < 8; col++)
@@ -39,59 +47,75 @@ namespace checkers
                         Height = tileSize,
                         FlatStyle = FlatStyle.Flat,
                         Location = new System.Drawing.Point(col * tileSize, row * tileSize),
-                        Tag = (row, col)  
+                        Tag = (row, col)
                     };
 
-                    
                     if ((row + col) % 2 == 1)
                     {
                         button.BackColor = Color.Black;
-                        button.ForeColor = Color.White;  
+                        button.ForeColor = Color.White;
                     }
                     else
                     {
                         button.BackColor = Color.White;
-                        button.Enabled = false;  
+                        button.Enabled = false;
                     }
 
-                    
                     button.Click += OnBoardButtonClick;
-
                     boardButtons[row, col] = button;
-                    this.Controls.Add(button);  
+                    this.Controls.Add(button);
                 }
             }
         }
 
-        
         private void OnBoardButtonClick(object sender, EventArgs e)
         {
             Button clickedButton = sender as Button;
-            var (row, col) = ((int row, int col))clickedButton.Tag;  
+            var (row, col) = ((int row, int col))clickedButton.Tag;
 
-            if (selectedPiece == null)  
+            if (selectedPiece == null)
             {
-               
-                if (game.board[row, col] != null)
+                // Selects a piece if it belongs to the current player
+                if (game.board[row, col] != null && game.board[row, col].Player == game.CurrentPlayer)
                 {
-                    selectedPiece = (row, col);  
-                    clickedButton.BackColor = Color.Yellow;  
+                    selectedPiece = (row, col);
+                    clickedButton.BackColor = Color.Yellow;
                 }
             }
-            else  
+            else
             {
                 var (selectedRow, selectedCol) = selectedPiece.Value;
 
-                
-                game.board[row, col] = game.board[selectedRow, selectedCol];  // Move the piece
-                game.board[selectedRow, selectedCol] = null;  // Clear the old position
+               
+                if (IsMoveValid(selectedRow, selectedCol, row, col))
+                {
+                    game.board[row, col] = game.board[selectedRow, selectedCol];  
+                    game.board[selectedRow, selectedCol] = null;  
 
-                selectedPiece = null;  
-                RenderBoard();  
+                    selectedPiece = null;
+                    RenderBoard();
+
+                    
+                    game.NextTurn();
+                    currentPlayerLabel.Text = "Current Player: Player " + game.CurrentPlayer;
+
+                    // Check for winner
+                    int winner = game.CheckForWinner();
+                    if (winner != 0)
+                    {
+                        MessageBox.Show("Player " + winner + " wins!");
+                        ResetGame();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid move. Try again.");
+                    selectedPiece = null;
+                    RenderBoard();
+                }
             }
         }
 
-       
         private void RenderBoard()
         {
             var board = game.GetBoard();
@@ -104,16 +128,15 @@ namespace checkers
                     if (piece != null)
                     {
                         if (piece.Player == 1)
-                            boardButtons[row, col].Text = "P1";  
+                            boardButtons[row, col].Text = "P1";
                         else if (piece.Player == 2)
-                            boardButtons[row, col].Text = "P2";  
+                            boardButtons[row, col].Text = "P2";
                     }
                     else
                     {
-                        boardButtons[row, col].Text = "";  
+                        boardButtons[row, col].Text = "";
                     }
 
-                    
                     if ((row + col) % 2 == 1)
                     {
                         boardButtons[row, col].BackColor = Color.Black;
@@ -121,7 +144,24 @@ namespace checkers
                 }
             }
         }
+
+        
+        private bool IsMoveValid(int startRow, int startCol, int endRow, int endCol)
+        {
+            
+            return Math.Abs(startRow - endRow) == 1 && Math.Abs(startCol - endCol) == 1;
+        }
+
+        // Reset the game 
+        private void ResetGame()
+        {
+            game = new CheckersGame();
+            selectedPiece = null;
+            currentPlayerLabel.Text = "Current Player: Player " + game.CurrentPlayer;
+            RenderBoard();
+        }
     }
+
 }
 
 
--- game_logic.cs
+++ game_logic.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace checkers
 {
     public class CheckersGame
     {
-        public Piece[,] board = new Piece[8, 8];  
+        public Piece[,] board = new Piece[8, 8];
+        public int CurrentPlayer { get; private set; } = 1;  
 
         public CheckersGame()
         {
-            
             InitializeBoard();
+        }
+
+        public void NextTurn()
+        {
+            CurrentPlayer = (CurrentPlayer == 1) ? 2 : 1;  
         }
 
         private void InitializeBoard()
@@ -23,21 +25,21 @@ namespace checkers
             {
                 for (int col = 0; col < 8; col++)
                 {
-                    if ((row + col) % 2 == 1)  
+                    if ((row + col) % 2 == 1)
                     {
-                        board[row, col] = new Piece(1);  
+                        board[row, col] = new Piece(1);
                     }
                 }
             }
 
-            
+           
             for (int row = 5; row < 8; row++)
             {
                 for (int col = 0; col < 8; col++)
                 {
                     if ((row + col) % 2 == 1)
                     {
-                        board[row, col] = new Piece(2);  
+                        board[row, col] = new Piece(2);
                     }
                 }
             }
@@ -47,12 +49,32 @@ namespace checkers
         {
             return board;
         }
+
+        public int CheckForWinner()
+        {
+            bool player1HasPieces = false;
+            bool player2HasPieces = false;
+
+            foreach (var piece in board)
+            {
+                if (piece != null)
+                {
+                    if (piece.Player == 1) player1HasPieces = true;
+                    if (piece.Player == 2) player2HasPieces = true;
+                }
+            }
+
+            if (!player1HasPieces) return 2;  // Player 2 wins if Player 1 has no pieces 
+            if (!player2HasPieces) return 1;  // Player 1 wins if Player 2 has no pieces 
+
+            return 0;  // No winner yet
+        }
     }
 
     public class Piece
     {
-        public int Player { get; private set; }  
-        public bool IsKing { get; set; }         
+        public int Player { get; private set; }
+        public bool IsKing { get; set; }
 
         public Piece(int player)
         {
